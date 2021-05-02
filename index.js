5a1bba8fb1a3f1c188f525122992e1e8d74b8e52
aeron34 <41888639+aeron34@users.noreply.github.com>
2021-05-02 02:17:10 -0400

    initial files

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use(express.static('Site1'));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/Site1/index.html');
+});
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'aerosemail12@gmail.com',
+    pass: '$Onic1991'
+  }
+});
+
+
+app.get('/buy', (req, res) => {
+  var mailOptions = {
+    from: 'aerosemail12@gmail.com',
+    to: 'sniperud@gmail.com',
+    subject:'Someone Bought.',
+    text: 'someone copped'
+  };
+
+  transporter.sendMail(mailOptions, function(err, info) {
+    console.log(`${err} ${info}`);
+    res.send('done')
+  })
+
+  res.send('done')
+})
+
+app.listen(3000)
